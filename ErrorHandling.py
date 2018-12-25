dd376b438c4dbead6cf86776a2c02255a4bac52d
akaicomet <45959235+akaicomet@users.noreply.github.com>
2018-12-25 01:18:20 -0500

    Create ErrorHandling.py
    Use this function - "exception" as decorator

---
+++ ErrorHandling.py
@@ -0,0 +1,29 @@
+!pip install tbvaccine 
+
+import functools
+import sys,traceback
+from tbvaccine import TBVaccine
+
+def exception(function):
+
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        #logger = create_logger()
+        try:
+            return function(*args, **kwargs)          
+        except Exception as e:
+            err = "There was an exception in \""
+            err += function.__name__ + "\" "
+            
+            print(err)
+            print(e)
+            print(type(e))
+            print(e.args)
+            print("message:{0}".format(e.with_traceback(sys.exc_info()[2])))
+            print(sys.last_value)
+            #traceback.print_last()
+            #traceback.print_exc()
+            # re-raise the exception
+            print(TBVaccine().format_exc())
+            raise
+    return wrapper
