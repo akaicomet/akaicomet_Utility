ed86146ab786f350ab5ab992444fb14680149546
akaicomet <45959235+akaicomet@users.noreply.github.com>
2018-12-30 14:43:00 -0500

    Update ErrorHandling.py
    Line 12: except TimeoutError: added

--- ErrorHandling.py
+++ ErrorHandling.py
@@ -8,23 +8,22 @@ def exception(function):
     def wrapper(*args, **kwargs):
         #logger = create_logger()
         try:
-            return function(*args, **kwargs)          
+            return function(*args, **kwargs)                   
+        except TimeoutError:
+            pass
         except Exception as e:
-            if e.errno == 2:
-                pass
-            else:
-                err = "There was an exception in \""
-                err += function.__name__ + "\" "
+            err = "There was an exception in \""
+            err += function.__name__ + "\" "
 
-                print(err)
-                print(e)
-                print(type(e))
-                print(e.args)
-                print("message:{0}".format(e.with_traceback(sys.exc_info()[2])))
-                print(sys.last_value)
-                #traceback.print_last()
-                #traceback.print_exc()
-                # re-raise the exception
-                print(TBVaccine().format_exc())
-                raise
+            print(err)
+            print(e)
+            print(type(e))
+            print(e.args)
+            print("message:{0}".format(e.with_traceback(sys.exc_info()[2])))
+            print(sys.last_value)
+            #traceback.print_last()
+            #traceback.print_exc()
+            # re-raise the exception
+            print(TBVaccine().format_exc())
+            raise
     return wrapper
