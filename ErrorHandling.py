1fa23f0d74336412424c868469726c33d36d8f9b
akaicomet <45959235+akaicomet@users.noreply.github.com>
2018-12-30 11:17:55 -0500

    Update ErrorHandling.py
    Line 13: if e.errno == 2: added. timeout error

--- ErrorHandling.py
+++ ErrorHandling.py
@@ -10,18 +10,21 @@ def exception(function):
         try:
             return function(*args, **kwargs)          
         except Exception as e:
-            err = "There was an exception in \""
-            err += function.__name__ + "\" "
-            
-            print(err)
-            print(e)
-            print(type(e))
-            print(e.args)
-            print("message:{0}".format(e.with_traceback(sys.exc_info()[2])))
-            print(sys.last_value)
-            #traceback.print_last()
-            #traceback.print_exc()
-            # re-raise the exception
-            print(TBVaccine().format_exc())
-            raise
+            if e.errno == 2:
+                pass
+            else:
+                err = "There was an exception in \""
+                err += function.__name__ + "\" "
+
+                print(err)
+                print(e)
+                print(type(e))
+                print(e.args)
+                print("message:{0}".format(e.with_traceback(sys.exc_info()[2])))
+                print(sys.last_value)
+                #traceback.print_last()
+                #traceback.print_exc()
+                # re-raise the exception
+                print(TBVaccine().format_exc())
+                raise
     return wrapper
